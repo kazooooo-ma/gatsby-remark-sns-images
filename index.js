400aaa02c36a66eb45179a9c00458c7aa9178b4e
yuu <uypsbss@gmail.com>
2020-04-03 14:16:38 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const catchy = require("catchy-image");
+
+module.exports = async ({ markdownNode }, pluginOptions) => {
+  // Sending gatsby-config.js settings and markdowns header metadata to image generation Library.
+  const result = await catchy.generate({
+    ...pluginOptions,
+    output: {
+      ...pluginOptions.output,
+      directory: `./public${markdownNode.fields.slug}`,
+      fileName: pluginOptions.output.fileName
+    },
+    meta: {
+      ...pluginOptions.meta,
+      title: markdownNode.frontmatter.title
+    }
+  });
+
+  console.info(`gatsby-remark-og-image: Successful generated: ${result}`);
+};
